ea7c041a1430bf12f32d08907e312d606fe0224f
wangxin-king <wangxin.king.314@gmail.com>
2017-09-26 10:55:00 +0800

    添加README.md使用说明

---
+++ src/driver/alipay/WapGateway.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: King
+ * Date: 2017/9/25
+ * Time: 15:55
+ */
+
+namespace king\payment\driver\alipay;
+
+
+class WapGateway extends Alipay
+{
+    public function getProductCode()
+    {
+        // TODO: Implement getProductCode() method.
+    }
+
+    public function getMethod()
+    {
+        // TODO: Implement getMethod() method.
+    }
+}
